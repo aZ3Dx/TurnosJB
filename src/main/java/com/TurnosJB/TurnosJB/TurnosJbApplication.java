c3851c4d0a86642c888fe7323b6ef388b55823a8
Jean Herrera <jeankevinherrera@gmail.com>
2024-08-29 20:13:08 -0500

    First commit

---
+++ src/main/java/com/TurnosJB/TurnosJB/TurnosJbApplication.java
@@ -0,0 +1,13 @@
+package com.TurnosJB.TurnosJB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnosJbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnosJbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TurnosJB/TurnosJB/TurnosJbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TurnosJB.TurnosJB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnosJbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
