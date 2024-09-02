e1c30483a84547681810d90f693cc44949b3e791
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-09-01 23:49:13 -0500

    Implementacion de los servicios

--- src/main/java/com/TurnosJB/TurnosJB/service/impl/PacienteServiceImpl.java
+++ src/main/java/com/TurnosJB/TurnosJB/service/impl/PacienteServiceImpl.java
@@ -1,4 +1,33 @@
 package com.TurnosJB.TurnosJB.service.impl;
 
-public class PacienteServiceImpl {
+import com.TurnosJB.TurnosJB.entity.Paciente;
+import com.TurnosJB.TurnosJB.service.IPacienteService;
+
+import java.util.List;
+
+public class PacienteServiceImpl implements IPacienteService {
+    @Override
+    public void guardar(Paciente paciente) {
+
+    }
+
+    @Override
+    public Paciente buscarPorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public void eliminar(Long id) {
+
+    }
+
+    @Override
+    public void actualizar(Paciente paciente) {
+
+    }
+
+    @Override
+    public List<Paciente> listar() {
+        return List.of();
+    }
 }
--- src/main/java/com/TurnosJB/TurnosJB/service/impl/TurnoServiceImpl.java
+++ src/main/java/com/TurnosJB/TurnosJB/service/impl/TurnoServiceImpl.java
@@ -1,4 +1,23 @@
 package com.TurnosJB.TurnosJB.service.impl;
 
-public class TurnoServiceImpl {
+import com.TurnosJB.TurnosJB.entity.Turno;
+import com.TurnosJB.TurnosJB.service.ITurnoService;
+
+import java.util.List;
+
+public class TurnoServiceImpl implements ITurnoService {
+    @Override
+    public Turno guardar(Turno turno) {
+        return null;
+    }
+
+    @Override
+    public Turno buscarPorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Turno> listarTodos() {
+        return List.of();
+    }
 }
