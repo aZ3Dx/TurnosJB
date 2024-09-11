06a311f6087c26f6ae5b90ad00bf4fb8efbb2246
Brayan Cespedes <bryancespedes1706@gmail.com>
2024-09-11 17:37:59 -0500

    log4j agregado

--- src/main/java/com/TurnosJB/TurnosJB/service/impl/OdontologoServiceImpl.java
+++ src/main/java/com/TurnosJB/TurnosJB/service/impl/OdontologoServiceImpl.java
@@ -10,6 +10,8 @@ import com.TurnosJB.TurnosJB.repository.ITurnoRepository;
 import com.TurnosJB.TurnosJB.service.IOdontologoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,6 +22,9 @@ public class OdontologoServiceImpl implements IOdontologoService {
     private final IOdontologoRepository iOdontologoRepository;
     private final ITurnoRepository iTurnoRepository;
 
+    // Importar el logger
+    private static final Logger LOGGER = LogManager.getLogger(OdontologoServiceImpl.class);
+
     @Autowired
     public OdontologoServiceImpl(IOdontologoRepository iOdontologoRepository, ITurnoRepository iTurnoRepository) {
         this.iOdontologoRepository = iOdontologoRepository;
@@ -28,16 +33,21 @@ public class OdontologoServiceImpl implements IOdontologoService {
 
     @Override
     public Odontologo guardar(Odontologo odontologo) throws ConflictException, BadRequestException {
+        LOGGER.info("Comenzamos a persistir un odontólogo");
+
+        LOGGER.info("Revisamos que no hayan odontólogos con la misma matrícula");
         // Revisamos que no hayan odontologos con el mismo matricula
         if (iOdontologoRepository.findByMatricula(odontologo.getMatricula()) != null) {
             throw new ConflictException("Ya existe un odontologo con la matricula " + odontologo.getMatricula());
         }
+        LOGGER.info("Revisamos que los campos de odontólogo no estén vacíos");
         // Revisamos que los campos de Odontologo no estén vacíos
         if (Optional.ofNullable(odontologo.getNombre()).orElse("").isBlank() ||
                 Optional.ofNullable(odontologo.getApellido()).orElse("").isBlank() ||
                 Optional.ofNullable(odontologo.getMatricula()).orElse("").isBlank()) {
             throw new BadRequestException("Los campos de odontologo no pueden estar vacíos");
         }
+        LOGGER.info("Este es el odontologo que se guardó " + odontologo.getNombre());
         return iOdontologoRepository.save(odontologo);
     }
 
@@ -45,8 +55,10 @@ public class OdontologoServiceImpl implements IOdontologoService {
     public Odontologo buscarPorId(Long id) throws ResourceNotFoundException {
         Optional<Odontologo> odontologo = iOdontologoRepository.findById(id);
         if (odontologo.isPresent()) {
+            LOGGER.info("El odontólogo encontrado es " + odontologo.get());
             return odontologo.get();
         } else {
+            LOGGER.error("No se encontró el odontólogo con id " + id);
             throw new ResourceNotFoundException("No se encontró el odontólogo con id " + id);
         }
     }
@@ -55,6 +67,7 @@ public class OdontologoServiceImpl implements IOdontologoService {
     public void eliminar(Long id) throws DataIntegrityViolationException {
         // Revisamos que el odontologo no tenga turnos asociados
         if (!iTurnoRepository.findByOdontologoId(id).isEmpty()) {
+            LOGGER.error("El odontólogo no puede ser eliminado porque tiene turnos asociados.");
             throw new DataIntegrityViolationException("El odontologo no puede ser eliminado porque tiene turnos asociados");
         }
         iOdontologoRepository.deleteById(id);
@@ -71,6 +84,7 @@ public class OdontologoServiceImpl implements IOdontologoService {
         if (Optional.ofNullable(odontologo.getNombre()).orElse("").isBlank() ||
                 Optional.ofNullable(odontologo.getApellido()).orElse("").isBlank() ||
                 Optional.ofNullable(odontologo.getMatricula()).orElse("").isBlank()) {
+            LOGGER.error("Los campos de odontólogos no pueden estar vacíos.");
             throw new BadRequestException("Los campos de Odontologo no pueden estar vacíos");
         }
         return iOdontologoRepository.save(odontologo);
@@ -78,11 +92,13 @@ public class OdontologoServiceImpl implements IOdontologoService {
 
     @Override
     public List<Odontologo> listar() {
+        LOGGER.info("Lista de todos los odontólogos");
         return iOdontologoRepository.findAll();
     }
 
     @Override
     public Odontologo buscarPorMatricula(String matricula) {
+        LOGGER.info("Se busca el odontólogo con matrícula " + matricula);
         return iOdontologoRepository.findByMatricula(matricula);
     }
 }
